a2a7faaa926d4ed9be9f7f79669af0e4998c0111
Burcu Bayraö <burju7@yahoo.com>
2023-06-05 20:12:43 +0300

    completed assigment

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio ? props.bio : " "}</p>
+      <p>{!props.bio === " " ? props.bio : " "}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
       <Links />
