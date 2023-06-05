3b67e0b2c1d3d82d41583b897792f5eb81085738
Burcu Bayraö <burju7@yahoo.com>
2023-06-05 20:07:29 +0300

    completed assigment

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Burcu",
+  city: "Bursa",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/sesamemaki",
+    linkedin: "https://www.linkedin.com/in/burcu-bayram-0a861459/",
   },
 };
 
